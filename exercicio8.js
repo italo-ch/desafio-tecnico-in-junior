2b1e78158202ae165f6f9d186cc8771dd0479441
italo-ch <italo_ide@id.uff.br>
2024-12-10 23:07:48 -0300

    add exercicio 8

---
+++ exercicio8.js
@@ -0,0 +1,89 @@
+const booksByCategory = [
+    {
+        category: "Romance",
+        books: [
+            {
+                title: "O Sol Também é Uma Estrela",
+                author: "Nicola Yoon",
+            },
+            {
+                title: "O Noivo da Minha Melhor Amiga",
+                author: "Emily Gin",
+            },
+            {
+                title: "Orgulho e Preconceito",
+                author: "Jane Austen",
+            },
+        ],
+    },
+    {
+        category: "Fantasia",
+        books: [
+            {
+                title: "Harry Potter e a Pedra Filosofal",
+                author: "J.K. Rowling",
+            },
+            {
+                title: 'O Hobbit',
+                author: 'J.R.R Tolkien',
+            },
+            {
+                title: 'Eragon',
+                author: 'Christopher Poolini',
+            },
+            {
+                title: 'O Nome do Vento',
+                author: 'Patrick Rothfuss',
+            }
+        ]
+    },
+    {
+        category: 'Suspense',
+        books: [
+            {
+                title: 'A Garota no Trem',
+                author: 'Paula Hawkins',
+            },
+            {
+                title: 'O Silêncio dos Inocentes',
+                author: 'Thomas Harris',
+            },
+        ]
+    },
+    {
+        category: 'Ficção Científica',
+        books: [
+            {
+                title: 'Duna',
+                author: 'Frank Herbert',
+            },
+            {
+                title: 'Neuromancer',
+                author: 'William Gibson',
+            },
+            {
+                title: 'Fundação',
+                author: 'Isaac Asimov',
+            },
+            {
+                title: '1984',
+                author: 'George Orwell',
+            },
+            {
+                title: 'O Fim da Infância',
+                author: 'Arthur C. Clarke',
+            },
+        ]
+    },
+];
+
+
+function countBooksInCategory() {
+    return booksByCategory.reduce((acc, {category, books}) => {
+        acc[category] = books.length;
+        return acc;
+    }, {})
+}
+
+console.log(booksByCategory);
+console.log(countBooksInCategory());
